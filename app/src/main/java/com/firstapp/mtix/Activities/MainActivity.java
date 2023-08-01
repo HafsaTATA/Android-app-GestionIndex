28a98bf9ef5691fef76a9f77b7573098ce4c24a8
HAFSA TATA <hafsatata.work@gmail.com>
2023-08-01 10:08:42 +0100

    version 1.1
    connected the project to my SQlite database and added the tests on the level of the login.Also designed the client layout and gave the app an icon

--- app/src/main/java/com/firstapp/mtix/Activities/MainActivity.java
+++ app/src/main/java/com/firstapp/mtix/Activities/MainActivity.java
@@ -3,14 +3,24 @@ package com.firstapp.mtix.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.firstapp.mtix.R;
 
 public class MainActivity extends AppCompatActivity {
+ ImageView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logout=findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
